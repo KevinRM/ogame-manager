e3734384664b6431bc18410fa126c8bbb5ca82a4
Kevin <lutipo@gmail.com>
2019-04-13 21:19:40 +0100

    Finished auto-transport resources and Attack alert

--- js/functions.js
+++ js/functions.js
@@ -6,14 +6,36 @@ $(document).ready(function () {
             case "recolectProduction": {
                 fnRecolectProduction();
             } break;
+            case "resetMemory": {
+                chrome.storage.local.clear(function () { });
+            }
         }
 
         sendResponse(true);
     });
 
-    chrome.storage.local.get("inProcessMethod", function (inProcessMethod) {
-        var nameMethod = inProcessMethod["inProcessMethod"];
+    chrome.storage.local.get(["inProcessMethod", "isUnderAttack"], function (savedData) {
+        var nameMethod = savedData["inProcessMethod"];
+        var isUnderAttack = savedData["isUnderAttack"];
 
+        // Check attack
+        fnCheckAttack();
+
+        var minRandMinutesIn = 5;
+        var maxRandMinutesEx = 13;
+        var nextCheck = 0;
+
+        if(isUnderAttack == undefined) {
+            nextCheck = Math.floor(minRandMinutesIn + Math.random() * (maxRandMinutesEx - minRandMinutesIn)) * 60000;
+        } else {
+            nextCheck = 15000;
+        }
+        console.log("Next check in: " + nextCheck / 60000 + " minutes (" + (nextCheck) + " ms)");
+        setInterval(() => {
+            document.location.reload();
+        }, nextCheck);
+
+        // Continue with method if correspond
         switch (nameMethod) {
             case "fnRecolectProduction": {
                 fnRecolectProduction();
@@ -28,40 +50,88 @@ function fnRecolectProduction() {
 
     planets = $(".planetlink");
 
-    chrome.storage.local.get(["currentIndexPlanet", "currentPage"], function (savedData) {
+    chrome.storage.local.get(["currentIndexPlanet", "currentPage", "goNextPlanet"], function (savedData) {
         var currentPlanet = savedData["currentIndexPlanet"];
         var currentPage = savedData["currentPage"];
+        var goNextPlanet = savedData["goNextPlanet"];
 
         // Si no estamos en la pagina de flota nos movemos a ella
-        console.log(currentPage);
-        if (currentPage == undefined || currentPage != "fleet1" /*y fleet 2 y fleet 3*/) {
-            fnGoFleetPage();
+        if (currentPage == undefined || currentPage != "fleet1" && currentPage != "fleet2" && currentPage != "fleet3") {
+            fnGoFleetPage1();
         } else {
             // Si no tenemos valor, empezamos en el primer planeta
             if (currentPlanet == undefined) {
                 currentPlanet = 0;
-                planets[currentPlanet].click();
-                //chrome.storage.local.set({ "currentIndexPlanet": currentPlanet }, function () { });
-            }
-
-            if (currentPlanet < planets.length - 1) {
-                currentPlanet++;
-
+                chrome.storage.local.set({ "currentIndexPlanet": currentPlanet, "goNextPlanet": false }, function () { });
                 planets[currentPlanet].click();
 
-                chrome.storage.local.set({ "currentIndexPlanet": currentPlanet }, function () { });
+            } else if (currentPlanet <= planets.length - 1) {
+                if (!goNextPlanet) {
+                    switch (currentPage) {
+                        case "fleet1": {
+                            chrome.storage.local.set({ "currentPage": "fleet2" }, function () { });
+
+                            var amountResources = parseInt($("#resources_metal").text().replace(/\./g, ""));
+                            amountResources += parseInt($("#resources_crystal").text().replace(/\./g, ""));
+                            amountResources += parseInt($("#resources_deuterium").text().replace(/\./g, ""));
+
+                            fnSendLargeCargoShips(amountResources);
+                        } break;
+                        case "fleet2": {
+                            chrome.storage.local.set({ "currentPage": "fleet3" }, function () { });
+
+                            document.querySelector("span[class*='planets'] a").click();
+                            document.querySelector("a[data-value*='2#28#10#3#L-8']").click();
+                            document.querySelector("#continue").click();
+                        } break;
+                        case "fleet3": {
+                            chrome.storage.local.set({ "currentPage": "fleet1", "goNextPlanet": true }, function () { });
+                            document.querySelector("#missionButton3").click();
+                            document.querySelector("#allresources").click();
+                            document.querySelector("#start").click();
+                        } break;
+                    }
+                } else {
+                    // Una vez terminado el proceso nos movemos al siguiente planeta
+                    currentPlanet++;
+                    chrome.storage.local.set({ "currentIndexPlanet": currentPlanet, "goNextPlanet": false }, function () { });
+
+                    if (currentPlanet > planets.length - 1) {
+                        document.location.reload();
+                    }
+
+                    planets[currentPlanet].click();
+                }
+
+                // =======================
+                // Hacer una clase de configuracion donde poner todas las palabras estas para tenerlas accesibles y cambiarlas en caso de cambien algun dia
+                // Tambien meter por ejemplo la capacidad de carga de las naves
+                // Poder introducir el planeta/luna de destino
+                // =======================
             } else {
-                fnClearStorage(["currentIndexPlanet", "inProcessMethod", "currentPage"]);
+                fnClearStorage(["currentIndexPlanet", "inProcessMethod", "currentPage", "goNextPlanet"]);
             }
         }
     });
 }
 
-function fnSendLargeCargoShips() {
+function fnSendLargeCargoShips(amountResources) {
+    var amountShips = amountResources / 35000;
+    var amountShipsInt = parseInt(amountShips.toString());
 
+    if (amountShipsInt > 0) {
+        var element = document.querySelector("#ship_203");
+
+        element.value = amountShipsInt;
+        element.dispatchEvent(new KeyboardEvent("keyup"));
+        document.querySelector("#continue").click();
+    } else {
+        chrome.storage.local.set({ "currentPage": "fleet1", "goNextPlanet": true }, function () { });
+        document.location.reload();
+    }
 }
 
-function fnGoFleetPage() {
+function fnGoFleetPage1() {
     chrome.storage.local.set({ "currentPage": "fleet1" }, function () { });
     $("a[href*='fleet1']")[0].click();
 }
@@ -71,9 +141,19 @@ function fnClearStorage(listKeys) {
         var error = chrome.runtime.lastError;
 
         if (error) {
-            console.log("Error => " + error);
+            alert("ERROR: " + error);
         } else {
-            console.log("Valores eliminados");
+            alert("Proceso completado");
         }
     })
+}
+
+function fnCheckAttack() {
+    if (document.querySelector("div[class*='soon']") != null) {
+        chrome.storage.local.set({ "isUnderAttack": true }, function () { });
+        var myAudio = new Audio(chrome.runtime.getURL("/resources/alert.wav"));
+        myAudio.play();
+    } else {
+        chrome.storage.local.remove(["isUnderAttack"], function () { });
+    }
 }
--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,19 @@
 $(document).ready(function () {
     var btnRecolectProduction = document.getElementById("recolectProduction");
+    var btnResetMemory = document.getElementById("resetMemory");
 
     $(btnRecolectProduction).click(function () {
-        $(btnRecolectProduction).attr("disabled", true);
-
         chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, "recolectProduction", function(response) {
                 console.log("Orden completada: " + response);
+            });
+        });
+    });
 
-                $(btnRecolectProduction).attr("disabled", false);
+    $(btnResetMemory).click(function () {
+        chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, "resetMemory", function(response) {
+                console.log("Orden completada: " + response);
             });
         });
     });
